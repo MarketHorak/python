8774186eec17eea2458e1eee573d32fbd4b095a5
market horak <margit@seznam.cz>
2020-04-09 21:21:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+pozadujeEuro = input("Zadej pocet Euro")
+
+pozadujeEuro = int(pozadujeEuro)
+kurz = 27.84
+cena = pozadujeEuro * kurz
+print(cena)
+
+hrubaMzda=60000
+hrubaMzda
+60000
+
+superhruba
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'superhruba' is not defined
+superhrubaMzda=1.34*0.15-2070
+superhrubaMzda
+-2069.799
+
+
+pozdrav= "ahoj"
+pozdrav
+'ahoj'
+
+x=1024
+z=72
+a=x/z
+a
+14.222222222222221
+
+>>> a=x%z
+  File "<stdin>", line 1
+    >>> a=x%z
+    ^
+SyntaxError: invalid syntax
+1024%72
+16
+delka=265
+delka/60=y
+  File "<stdin>", line 1
+SyntaxError: cannot assign to operator
+delka//60=y
+  File "<stdin>", line 1
+SyntaxError: cannot assign to operator
+hodin=60
+
+delka/hodin=a
+  File "<stdin>", line 1
+SyntaxError: cannot assign to operator
+trvani= delka/hodin
+trvani
+4.416666666666667
+minut=delka%hodin
+minut
+25
+round(3,24)
+3
+round(4,5)
+4
+round(4,6)
+4
+prumer znamek = 2,38
+  File "<stdin>", line 1
+    prumer znamek = 2,38
+           ^
+SyntaxError: invalid syntax
+round = 2.389
+prumer znamek = 2.38
